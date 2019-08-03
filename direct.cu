505a8ae646da351f08e05abc92266e9a29d9309f
Rio Yokota <rioyokota@gmail.com>
2019-08-03 17:38:25 +0900

    Forward declaration

--- direct.cu
+++ direct.cu
@@ -13,16 +13,18 @@ double get_time() {
 
 __global__ void GPUkernel(int N, double * x, double * y, double * z, double * m,
 			  double * ax, double * ay, double * az, double G, double eps) {
-  int i = blockIdx.x * THREADS + threadIdx.x;
-  double axi = 0;
-  double ayi = 0;
-  double azi = 0;
-  double xi = x[i];
-  double yi = y[i];
-  double zi = z[i];
-  double Gmi = G * m[i];
+  int i, j, jb;
+  double axi, ayi, azi, xi, yi, zi, Gmi, dx, dy, dz, R2, invR, invR3;
+  i = blockIdx.x * THREADS + threadIdx.x;
+  axi = 0;
+  ayi = 0;
+  azi = 0;
+  xi = x[i];
+  yi = y[i];
+  zi = z[i];
+  Gmi = G * m[i];
   __shared__ double xj[THREADS], yj[THREADS], zj[THREADS], mj[THREADS];
-  for ( int jb=0; jb<N/THREADS; jb++ ) {
+  for ( jb=0; jb<N/THREADS; jb++ ) {
     __syncthreads();
     xj[threadIdx.x] = x[jb*THREADS+threadIdx.x];
     yj[threadIdx.x] = y[jb*THREADS+threadIdx.x];
@@ -30,13 +32,13 @@ __global__ void GPUkernel(int N, double * x, double * y, double * z, double * m,
     mj[threadIdx.x] = m[jb*THREADS+threadIdx.x];
     __syncthreads();
 #pragma unroll
-    for( int j=0; j<THREADS; j++ ) {
-      double dx = xi - xj[j];
-      double dy = yi - yj[j];
-      double dz = zi - zj[j];
-      double R2 = dx * dx + dy * dy + dz * dz + eps;
-      double invR = rsqrtf(R2);
-      double invR3 = invR * invR * invR * Gmi * mj[j];
+    for( j=0; j<THREADS; j++ ) {
+      dx = xi - xj[j];
+      dy = yi - yj[j];
+      dz = zi - zj[j];
+      R2 = dx * dx + dy * dy + dz * dz + eps;
+      invR = rsqrtf(R2);
+      invR3 = invR * invR * invR * Gmi * mj[j];
       axi -= dx * invR3;
       ayi -= dy * invR3;
       azi -= dz * invR3;
@@ -51,11 +53,12 @@ int main() {
 // Initialize
   int N = 1 << 16;
   int i, j;
-  double OPS = 20. * N * N * 1e-9;
-  double G = 6.6743e-11;
-  double EPS = 1e-4;
-  double tic, toc;
+  double OPS, G, eps, Gmi, tic, toc, diff, norm;  
+  double axi, ayi, azi, dx, dy, dz, R2, invR, invR3;
   double *x, *y, *z, *m, *ax, *ay, *az;
+  OPS = 20. * N * N * 1e-9;
+  G = 6.6743e-11;
+  eps = 1e-4;
   cudaMallocManaged((void**)&x, N * sizeof(double));
   cudaMallocManaged((void**)&y, N * sizeof(double));
   cudaMallocManaged((void**)&z, N * sizeof(double));
@@ -73,27 +76,28 @@ int main() {
 
 // CUDA
   tic = get_time();
-  GPUkernel<<<N/THREADS,THREADS>>>(N, x, y, z, m, ax, ay, az, G, EPS);
+  GPUkernel<<<N/THREADS,THREADS>>>(N, x, y, z, m, ax, ay, az, G, eps);
   cudaThreadSynchronize();
   toc = get_time();
   printf("CUDA   : %e s : %lf GFlops\n",toc-tic, OPS/(toc-tic));
 
 // No CUDA
-  double diff = 0, norm = 0;
+  diff = 0;
+  norm = 0;
   tic = get_time();
-#pragma omp parallel for private(j) reduction(+: diff, norm)
+#pragma omp parallel for private(axi,ayi,azi,Gmi,j,dx,dy,dz,R2,invR,invR3) reduction(+: diff, norm)
   for (i=0; i<N; i++) {
-    double axi = 0;
-    double ayi = 0;
-    double azi = 0;
-    double Gmi = G * m[i];
+    axi = 0;
+    ayi = 0;
+    azi = 0;
+    Gmi = G * m[i];
     for (j=0; j<N; j++) {
-      double dx = x[i] - x[j];
-      double dy = y[i] - y[j];
-      double dz = z[i] - z[j];
-      double R2 = dx * dx + dy * dy + dz * dz + EPS;
-      double invR = 1.0f / sqrtf(R2);
-      double invR3 = invR * invR * invR * Gmi * m[j];
+      dx = x[i] - x[j];
+      dy = y[i] - y[j];
+      dz = z[i] - z[j];
+      R2 = dx * dx + dy * dy + dz * dz + eps;
+      invR = 1.0f / sqrtf(R2);
+      invR3 = invR * invR * invR * Gmi * m[j];
       axi -= dx * invR3;
       ayi -= dy * invR3;
       azi -= dz * invR3;
