d66d9d5d61ffd10207b1a4be6c75f82d09fbf837
Rio Yokota <rioyokota@gmail.com>
2019-08-22 09:25:00 +0900

    Finished mpi_direct.cu

--- mpi_direct.cu
+++ mpi_direct.cu
@@ -5,11 +5,11 @@
 #include <sys/time.h>
 #include <assert.h>
 
-__global__ void GPUkernel(int N, double * x, double * y, double * z, double * m,
+__global__ void GPUkernel(int begin, int N, double * x, double * y, double * z, double * m,
 			  double * ax, double * ay, double * az, double G, double eps) {
   int i, j, jb;
   double axi, ayi, azi, xi, yi, zi, dx, dy, dz, R2, invR, invR3;
-  i = blockIdx.x * blockDim.x + threadIdx.x;
+  i = blockIdx.x * blockDim.x + threadIdx.x + begin;
   axi = 0;
   ayi = 0;
   azi = 0;
@@ -58,7 +58,7 @@ int main(int argc, char **argv) {
   int N, threads, mpisize=0, mpirank=0, gpusize=0, gpurank=0;
   struct timeval tic, toc;
   double OPS, G, eps, time;  
-  double *x, *y, *z, *m, *ax, *ay, *az;
+  double *x, *y, *z, *m, *ax, *ay, *az, *axg, *ayg, *azg;
   FILE *file;
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
@@ -66,19 +66,12 @@ int main(int argc, char **argv) {
   cudaGetDeviceCount(&gpusize);
   cudaSetDevice(mpirank % gpusize);
   cudaGetDevice(&gpurank);
-  for (int irank=0; irank<mpisize; irank++) {
-    MPI_Barrier(MPI_COMM_WORLD);
-    if (mpirank == irank) {
-      printf("MPI rank    : %d / %d  GPU device : %d / %d\n",
-             mpirank, mpisize, gpurank, gpusize);
-    }
-  }
   if ( (file = fopen("initial.dat","rb")) == NULL ) {
     fprintf(stderr, "File open error.\n");
     exit(EXIT_FAILURE);
   }
   assert( fread(&N,sizeof(int),1,file) == 1 );
-  OPS = 20. * N * N * 1e-9;
+  OPS = 20. * N * N / mpisize * 1e-9;
   cudaMallocManaged((void**)&x, N * sizeof(double));
   cudaMallocManaged((void**)&y, N * sizeof(double));
   cudaMallocManaged((void**)&z, N * sizeof(double));
@@ -86,6 +79,9 @@ int main(int argc, char **argv) {
   cudaMallocManaged((void**)&ax, N * sizeof(double));
   cudaMallocManaged((void**)&ay, N * sizeof(double));
   cudaMallocManaged((void**)&az, N * sizeof(double));
+  cudaMallocManaged((void**)&axg, N * sizeof(double));
+  cudaMallocManaged((void**)&ayg, N * sizeof(double));
+  cudaMallocManaged((void**)&azg, N * sizeof(double));
   assert( fread(x,sizeof(double),N,file) == N );
   assert( fread(y,sizeof(double),N,file) == N );
   assert( fread(z,sizeof(double),N,file) == N );
@@ -96,17 +92,28 @@ int main(int argc, char **argv) {
   int begin = 0;
   int end = N;
   splitRange(begin, end, mpirank, mpisize);
+  for (int irank=0; irank<mpisize; irank++) {
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (mpirank == irank) {
+      //printf("MPI rank    : %d / %d  GPU device : %d / %d, begin: %d, end: %d\n",
+      //       mpirank, mpisize, gpurank, gpusize, begin, end);
+    }
+  }
   gettimeofday(&tic,NULL);
-  GPUkernel<<<N/threads,threads,threads*4*sizeof(double)>>>(N, x, y, z, m, ax, ay, az, G, eps);
+  assert((end-begin)%threads == 0);
+  GPUkernel<<<(end-begin)/threads,threads,threads*4*sizeof(double)>>>(begin, N, x, y, z, m, ax, ay, az, G, eps);
   cudaThreadSynchronize();
   gettimeofday(&toc,NULL);
   time = toc.tv_sec-tic.tv_sec+(toc.tv_usec-tic.tv_usec)*1e-6;
-  printf("GPU    : %e s : %lf GFlops\n",time, OPS/time);
-  file = fopen("direct.dat","wb");
+  if (mpirank ==0) printf("GPU    : %e s : %lf GFlops\n",time, OPS/time);
+  MPI_Allreduce(ax,axg,N,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
+  MPI_Allreduce(ay,ayg,N,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
+  MPI_Allreduce(az,azg,N,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
+  file = fopen("approx.dat","wb");
   fwrite(&N,sizeof(int),1,file);
-  fwrite(ax,sizeof(double),N,file);
-  fwrite(ay,sizeof(double),N,file);
-  fwrite(az,sizeof(double),N,file);
+  fwrite(axg,sizeof(double),N,file);
+  fwrite(ayg,sizeof(double),N,file);
+  fwrite(azg,sizeof(double),N,file);
   fclose(file);
   cudaFree(x);
   cudaFree(y);
@@ -115,6 +122,9 @@ int main(int argc, char **argv) {
   cudaFree(ax);
   cudaFree(ay);
   cudaFree(az);
+  cudaFree(axg);
+  cudaFree(ayg);
+  cudaFree(azg);
   MPI_Finalize();
   return 0;
 }
